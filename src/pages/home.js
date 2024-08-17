12de6ca91670ec62f9d00c5d2b84b45982645a3d
kpatel115 <kpatel9338448@gmail.com>
2024-08-17 09:42:23 -0600

    Created Links to different pages on Home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import
+import React from 'react';
+import { Link } from 'react-router-dom;'
 
 const Home = () => {
+
   return (
     <div>
       <h1>Notedly</h1>
       <p>This is the home page</p>
+      <ul>
+        <li>
+          <Link to="/MyNotes">MyNotes</Link>
+        </li>
+        <li>
+          <Link to="/Favorites">favorites</Link>
+        </li>
+      </ul>
     </div>
   )
 }
 
-export default Home
+export default Home;
