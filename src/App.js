4bd25c02e24fd8c7334302f5dc4fcdf66fdfa844
kpatel115 <kpatel9338448@gmail.com>
2024-08-19 14:46:01 -0600

    Trouble with Querying the API onto the UI

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 /* Pages */
-import Home from './pages/home';
+import Home from './pages/Home';
 import MyNotes from './pages/MyNotes';
 import Favorites from './pages/Favorites';
 /* Styling */
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,48 @@
 import React from 'react';
 
-const home = () => {
+import { useQuery, gql } from '@apollo/client';
+const GET_NOTES = gql`
+query noteFeed($cursor: String) {
+  noteFeed(cursor: $cursor) {
+    cursor
+    hasNextPage
+    notes {
+      id
+      createdAt
+      content
+      favoriteCount
+      author {
+        username
+        id
+        avatar
+      }
+    }
+  }
+}
+`;
 
+const Home = () => {
+  // useQuery Apollo React Hook
+  const { data, loading, error } = useQuery(GET_NOTES);
+
+  // if data is loading display message
+  if(loading) return <p>Loading...</p>;
+  // if error fetching data, display message
+  if(error) return <p>Error!</p>;
+  // If data is successful, display Data to Client UI
   return (
     <div>
-      <h1 className=''>Notedly - Welcome</h1>
-      <p>This is the home page</p>
+      {/* <h1 className=''>Notedly - Welcome</h1>
+      <p>This is the home page</p> */}
+      {console.log(data)}
+      <h3>Data has been loaded</h3>
+      <div>
+        {data.noteFeed.notes.map(note => (
+          <div key={note.id}>{note.author.username}</div>
+        ))}
+      </div>
     </div>
   )
 }
 
-export default home;
+export default Home;
