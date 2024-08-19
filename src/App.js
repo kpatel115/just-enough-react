cf1b1b41f72a383bbf1f868f7018abfe77e29715
kpatel115 <kpatel9338448@gmail.com>
2024-08-19 12:50:28 -0600

    added gitignore, noticed problem with App.js - function named Pages instead of App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
+/* React */
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+/* Apollo Client Libraries */
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 /* Components */
 import Header from './components/Header';
 import Navbar from './components/Navbar';
-
-
 /* Pages */
 import Home from './pages/home';
 import MyNotes from './pages/MyNotes';
 import Favorites from './pages/Favorites';
-
 /* Styling */
 import './App.css';
 
 const Pages = () => {
+  const 
   return (
     <Router>
       <Header/>
